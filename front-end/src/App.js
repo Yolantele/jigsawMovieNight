f31074b775b4a46450497120988e5651f016ee34
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2018-01-26 11:43:01 +0000

    rob: created movies component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
@@ -9,6 +10,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"></h1>
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ front-end/src/components/Movies/Movies.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Movies.css";
+import ReactDOM from "react-dom";
+
+
+class Movies extends React.Component {
+  
+}
